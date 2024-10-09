cb97b42629098f8375a6e31e2e1d9ba06cece77a
Gustavo Ramirez <jungdrew@yahoo.com>
2024-10-09 18:45:11 -0500

    Agrega vistas de inicio de sesión, registro, términos y condiciones, y olvido contraseña.

---
+++ client/src/lib/shared/ForgotForm.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    let email = '';
+
+    function handleSubmit() {
+        // Handle registration logic here
+        console.log('Request submitted', { email });
+    }
+
+    function switchToLogin() {
+        dispatch('switchForm', 'login');
+    }
+</script>
+
+<form on:submit|preventDefault={handleSubmit} class="edunat__form">
+    <h2>Recuperación de Clave</h2>
+
+    <div class="edunat__form-field">
+        <div>Email</div>
+        <input
+            type="email"
+            class="edunat__input"
+            placeholder="Ingresa tu Email"
+            bind:value={email}
+            required
+        />
+    </div>
+
+    <button class="edunat__btn" type="submit">Enviar</button>
+    <p>¿Ya tienes una cuenta? <button class="edunat__link" on:click|preventDefault|stopPropagation={switchToLogin}>Inicia sesión</button></p>
+</form>
--- client/src/routes/about/+page.svelte
+++
@@ -1,5 +0,0 @@
-<!-- <script lang="ts">
-  import Icon from "$lib/Icon.svelte";
-</script>
-
-<Icon size=32 icon="face"/> -->
